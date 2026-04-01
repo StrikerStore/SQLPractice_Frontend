e72b4ea94564ee271351fceca83fbe66b8f4f916
StrikerStore <strikerstore07@gmail.com>
2026-04-02 01:05:49 +0530

    v1 frontend

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -445,7 +445,7 @@ export default function Workspace() {
                     </button>
                   ))}
                 </div>
-                <div className="flex gap-1.5">
+                <div className="flex flex-col gap-1.5">
                   <select
                     value={filterDb}
                     onChange={(e) => setFilterDb(e.target.value)}
